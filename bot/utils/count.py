5bfc5a02a1240755fe6f17d57cd5f1abcc57cbb2
yummy1gay <artem.okunkov228@gmail.com>
2024-11-08 08:41:37 +0100

    .

--- bot/utils/count.py
+++ bot/utils/count.py
@@ -10,11 +10,23 @@ banned_count = 0
 for filename in os.listdir('data'):
     if filename.endswith('.json'):
         file_path = os.path.join('data', filename)
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            total_balance += data.get('balance', 0)
-            if data.get('banned', False):
-                banned_count += 1
+        
+        if os.path.getsize(file_path) == 0:
+            continue
+        
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+
+                total_balance += data.get('balance', 0)
+
+                if data.get('banned', False):
+                    banned_count += 1
+                    
+        except json.JSONDecodeError:
+            pass
+        except Exception as e:
+            pass
 
 logger.info(f'💰 Total Balance: {total_balance}')
 logger.info(f'😨 Number of <red>banned</red> sessions (in @WheelOfWhalesBot): {banned_count}')
